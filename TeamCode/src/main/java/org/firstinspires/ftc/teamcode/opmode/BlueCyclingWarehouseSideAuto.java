2eaa41f18e9d10755705b3089e3bfbc94a0add6d
Daniel Goz <daniel@dgoz.net>
2022-02-19 11:06:26 -0500

    Disabled cycling autos because controlling penalties

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/BlueCyclingWarehouseSideAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/BlueCyclingWarehouseSideAuto.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.opmode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 import org.opencv.core.Point;
 
+@Disabled
 @Autonomous
 public class BlueCyclingWarehouseSideAuto extends CyclingWarehouseSideAuto {
     public BlueCyclingWarehouseSideAuto() {
